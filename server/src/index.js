6518c7f54afdd0b8fb842585e1e7a8384f8cf9d9
zdd <zddhub@gmail.com>
2021-07-11 16:30:13 +0800

    Add mocked data

--- server/src/index.js
+++ server/src/index.js
@@ -3,9 +3,28 @@
 import { ApolloServer } from "apollo-server";
 import typeDefs from "./schema.js";
 
+
+const mocks = {
+  Track: () => ({
+    id: () => 'track_01',
+    title: () => 'Astro Kitty, Space Explorer',
+    author: () => {
+      return {
+        name: 'Grumpy Cat',
+        photo:
+          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg'
+      };
+    },
+    thumbnail: () =>
+      'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
+    length: () => 1210,
+    modulesCount: () => 6
+  })
+};
+
 const server = new ApolloServer({
   typeDefs,
-  mocks: true
+  mocks
 })
 
 server.listen().then(() => {
