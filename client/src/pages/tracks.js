95674681470586a73f93491d99fba48cfc7ec350
zdd <zddhub@gmail.com>
2021-07-11 17:17:28 +0800

    Add query result

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Layout } from '../components';
 import { gql, useQuery } from '@apollo/client';
 import TrackCard from '../containers/track-card';
+import QueryResult from '../components/query-result';
 
 
 /** TRACKS query to retrieve all tracks */
@@ -27,15 +28,14 @@ export const TRACKS = gql`
  */
 const Tracks = () => {
   const {loading, error, data} = useQuery(TRACKS);
-  if (loading) return 'Loading...';
-  if (error) return `Error! ${error.message}`;
-
   return <Layout grid>
+    <QueryResult error={error} loading={loading} data={data}>
     {
       data?.tracksForHome?.map(track => (
         <TrackCard key={track.id} track={track} />
       ))
     }
+    </QueryResult>
   </Layout>;
 };
 
